2072a2fd72b801aa50bb125645de02cba6f31f70
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-05-10 16:41:03 +1000

    Tidy up

--- src/openscm_twolayermodel/impulse_response_model.py
+++ src/openscm_twolayermodel/impulse_response_model.py
@@ -1,5 +1,20 @@
+"""
+Module containing the impulse response model
+
+The 2-timescale impulse response model is mathematically equivalent to the
+two-layer model without state dependence.
+"""
 from .base import Model
 
 
 class ImpulseResponseModel(Model):
-    pass
+    """
+    TODO: top line and paper references
+
+    This implementation uses a forward-differencing approach. This means that
+    temperature and ocean heat uptake values are start of timestep values. For
+    example, temperature[i] is only affected by drivers from the i-1 timestep.
+    In practice, this means that the first temperature and ocean heat uptake
+    values will always be zero and the last value in the input drivers has no
+    effect on model output.
+    """
--- tests/integration/test_model_integration_base.py
+++ tests/integration/test_model_integration_base.py
@@ -1,8 +1,4 @@
 from abc import ABC, abstractmethod
-from unittest.mock import MagicMock
-
-import pytest
-from openscm_units import unit_registry as ur
 
 
 class ModelIntegrationTester(ABC):
