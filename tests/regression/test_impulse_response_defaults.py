e165c2671c4fdc4b2583cb18945e53cff758d880
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-05-12 21:53:34 +1000

    Add impulse response regression tests

---
+++ tests/regression/test_impulse_response_defaults.py
@@ -0,0 +1,19 @@
+import os.path
+
+from openscm_units import unit_registry as ur
+
+from openscm_twolayermodel import ImpulseResponseModel
+
+
+def test_impulse_response_defaults(
+    update_expected_files,
+    test_rcmip_forcings_scmrun,
+    test_twolayer_output_dir,
+    run_model_output_comparison,
+):
+    impulse_response_default = ImpulseResponseModel()
+    res = impulse_response_default.run_scenarios(test_rcmip_forcings_scmrun)
+
+    expected = os.path.join(test_twolayer_output_dir, "test_impulse_response_defaults.csv")
+
+    run_model_output_comparison(res, expected, update_expected_files)
