Fix heat uptake calculation in impulse response model

src/openscm_twolayermodel/impulse_response_model.py
@@ -7,7 +7,7 @@ two-layer model without state dependence.
 import numpy as np
 from openscm_units import unit_registry as ur
 
-from .base import TwoLayerVariant
+from .base import TwoLayerVariant, _calculate_geoffroy_helper_parameters
 from .constants import density_water, heat_capacity_water
 from .errors import ModelStateError
 
@@ -223,25 +223,14 @@ class ImpulseResponseModel(
         if np.equal(efficacy, 1):
             efficacy_term = 0 * ur(self._erf_unit)
         else:
-            eta = two_layer_paras["eta"]
-
-            C = two_layer_paras["du"] / (heat_capacity_water * density_water)
-            C_D = two_layer_paras["dl"] / (heat_capacity_water * density_water)
-
-            b_pt_1 = (lambda_0 + efficacy * eta) / C
-            b_pt_2 = eta / C_D
-            b = b_pt_1 + b_pt_2
-            b_star = b_pt_1 - b_pt_2
-            delta = b ** 2 - (4 * lambda_0 * eta) / (C * C_D)
-
-            phicoeff = C / (2 * efficacy * eta)
-            phi1 = phicoeff * (b_star - delta ** 0.5)
-            phi2 = phicoeff * (b_star + delta ** 0.5)
+            gh = _calculate_geoffroy_helper_parameters(
+                two_layer_paras["du"], two_layer_paras["dl"], two_layer_paras["lambda_0"], two_layer_paras["efficacy"], two_layer_paras["eta"]
+            )
 
             t1_h = t1 * ur(self._temp1_unit)
             t2_h = t2 * ur(self._temp2_unit)
             efficacy_term = (
-                eta * (efficacy - 1) * ((1 - phi1) * t1_h - (1 - phi2) * t2_h)
+                two_layer_paras["eta"] * (efficacy - 1) * ((1 - gh["phi1"]) * t1_h - (1 - gh["phi1"]) * t2_h)
             )
 
             if str(efficacy_term.units) != "watt / meter ** 2":