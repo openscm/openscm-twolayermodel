028c3b5965106dff31ac4ab00c2e9daf67be34b7
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-07-31 12:36:21 +1000

    Fix pylint

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ REQUIREMENTS_DEV = [
         "black",
         "black-nb",
         "flake8",
-        "isort",
+        "isort<5",  # isort 5 incompatible with pylint
         "mypy",
         "nbdime",
         "pydocstyle",
